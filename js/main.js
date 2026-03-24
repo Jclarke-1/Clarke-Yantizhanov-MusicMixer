156a24dbc84c66476af15b6f5335077203c3ac4a
Jclarke-1 <j_clarke226924@fanshaweonline.ca>
2026-03-24 17:51:57 -0400

    Added Audio Controls
    added audio controls in JS, needs finetuning and to make sure audio plays with the correct dataset according to which seals are placed within the dropzone. Pause, play and restart buttons work though. Will be back to fine tune

--- js/main.js
+++ js/main.js
@@ -6,11 +6,28 @@ const targetZones = document.querySelectorAll(".target-zone");
 let currentDragged = null;
 const resetBttn = document.querySelector(".reset-btn");
 const sealBox = document.querySelector(".sealboxes");
-const theAudioEl = document.querySelectorAll(".seal-audio");
+const playBttn = document.querySelector("#playButton");
+const pauseBttn = document.querySelector("#pauseButton");
+const audioSrc = document.querySelectorAll("audio");
+
+myaudios = [
+{species: 'baikal', src: 'audio/baikal-seal'.wav}, 
+{species: 'elephant', src: 'audio/elephant-seal'.wav},
+{species: 'gray', src: 'audio/gray-seal'.wav},
+{species: 'harbor', src: 'audio/harbor-seal'.wav}, 
+{species: 'ribbon', src: 'audio/ribbon_seal'.wav},
+{species: 'lion', src: 'audio/sea-lion'.wav}
+];
+
+finalElements = ''
+myaudios.forEach(audio => {
+  finalElements += `<audio data-species=${audio.species} src=${audio.src}></audio>`
+})
+
+
 
 
 //Functions
-
 
 function dragStart() {
     console.log("Drag Start");
@@ -32,15 +49,39 @@ function dropped(e) {
     currentDragged = null;
 }
 
+function loadAudio() {
+    let currentSrc = `audio/${this.dataset.species}.wav`;
+    audioSrc.src = currentSrc;
+    audioSrc.load();
+    playAudio();
+}
+
+function playAudio() {
+    audioSrc.forEach(wav => {
+     wav.currentTime = 0; 
+    wav.play();
+    })
+
+}
+
+function pauseAudio() {
+    audioSrc.forEach(audioStart => {
+        audioStart.pause()
+    })
+}
+
 
 function resetPlace() {
     targetZones.forEach(zone => {
     if(zone.firstElementChild) {
         sealBox.appendChild(zone.firstElementChild);
     }
+    audioSrc.currentTime = 0;
+    audioSrc.pause();
     });
 }
 //Eventlisteners
+
 
 seals.forEach(seal => {
     seal.addEventListener("dragstart", dragStart);
@@ -52,3 +93,7 @@ targetZones.forEach(zone => {
 });
 
 resetBttn.addEventListener("click", resetPlace);
+
+playBttn.addEventListener("click", playAudio);
+
+pauseBttn.addEventListener("click", pauseAudio);
